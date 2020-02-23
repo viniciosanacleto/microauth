d77eaceb20153b4b226f07a7f008e860ab991586
Vinicios Anacleto <viniciosanacleto@hotmail.com>
2020-02-23 12:53:40 -0300

    - Refactor of UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,45 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use App\Repositories\UserRepository;
+use App\Models\User;
 use App\Tasks\User\CreateUserTask;
-use App\Tasks\User\DeleteUserTask;
-use App\Tasks\User\UpdateUserTask;
-use common\utils\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     public function create(Request $request)
     {
-        $createUserTask = new CreateUserTask($request->input());
-        return Response::buildFromTask($createUserTask);
-    }
+        $validation = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+        if ($validation->fails()) {
+            return response(['errors' => $validation->errors(), 'message' => 'Failed to create a new User.'], 400);
+        }
 
-//    public function update(Request $request, $id)
-//    {
-//        $updateUserTask = new UpdateUserTask($id, $request->input());
-//        return Response::buildFromTask($updateUserTask);
-//    }
-//
-//    public function delete($id)
-//    {
-//        $deleteUserTask = new DeleteUserTask($id);
-//        return Response::buildFromTask($deleteUserTask);
-//    }
-//
-//    public function getAll(Request $request){
-//        $repository = new UserRepository();
-//        return Response::buildFromRepository([$repository, 'getAll'],$request->input('limit'));
-//    }
-//
-//    public function getById($id){
-//        $repository = new UserRepository();
-//        return Response::buildFromRepository([$repository,'getById'],$id);
-//    }
-//
-//    public function getAllRandomCode(){
-//        $repository = new UserRepository();
-//        return Response::buildFromRepository([$repository,'getAllWithRandomCode']);
-//    }
+        try {
+            User::create($request->all());
+            return response(['message' => 'User successfully created!']);
+        } catch (\Exception $e) {
+            return response(['errors' => $e->getMessage(), 'message' => 'Failed to create a new User.'], 500);
+        }
+    }
 }
